0c89934b49c8910269e8c8a77647cf93ec6153a1
Jon <jcdiem3@gmail.com>
2019-12-10 00:51:19 -0600

    janky unit testing -Finalised JUnit --It has 'some quirks' -Ready to submit

--- src/us/jlp/test/EventCheckoutTest.java
+++ src/us/jlp/test/EventCheckoutTest.java
@@ -1,6 +1,11 @@
 package us.jlp.test;
 
 import org.junit.jupiter.api.Test;
+import us.jlp.CarTicket;
+import us.jlp.CheckoutStrategy;
+import us.jlp.EventCheckout;
+
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,9 +13,13 @@ class EventCheckoutTest {
 
     @Test
     void reportTicket() {
+        CarTicket testTicket = new CarTicket(3, LocalTime.of(17,59,59));
+        CheckoutStrategy checkoutTest = new EventCheckout();
+
+        int reportedCost = checkoutTest.reportTicket(testTicket,LocalTime.now()); //Flat rate, so out time doesn't matter aside from documentation, which isn't tested here
+        int expectedCost = checkoutTest.getBaseCost();
+        assertEquals(expectedCost,reportedCost,"Flat rate failed, should be "+expectedCost+" according to BASECOST, but got "+reportedCost);
     }
 
-    @Test
-    void getBaseCost() {
-    }
+    /* Not adding a test for getters of finals */
 }
--- src/us/jlp/test/NormalCheckoutTest.java
+++ src/us/jlp/test/NormalCheckoutTest.java
@@ -1,14 +1,35 @@
 package us.jlp.test;
+//TODO: Fix IntelliJ thinking this directory is a package so I can stop adding 9001 imports
+import org.junit.jupiter.api.Test;
+import us.jlp.CarTicket;
+import us.jlp.CheckoutStrategy;
+import us.jlp.NormalCheckout;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NormalCheckoutTest {
 
-    @org.junit.jupiter.api.Test
-    void reportTicket() {
+    @Test
+    void reportTicketThreeHour() {//TEST 1 - The first 3 hours
+        CarTicket testTicket = new CarTicket(3,LocalTime.of(17,59,59));
+        CheckoutStrategy checkoutTest = new NormalCheckout();
+
+        int reportedPrice = checkoutTest.reportTicket(testTicket,LocalTime.of(20,59,59)); //3 hours, so price should be BASECOST according to formula
+        int expectedPrice = checkoutTest.getBaseCost(); //For easier reading
+        assertEquals(expectedPrice,reportedPrice,"first 3 hours are not BASECOST like they should be, got "+reportedPrice+" from  method call instead of "+checkoutTest.getBaseCost());
     }
 
-    @org.junit.jupiter.api.Test
-    void getBaseCost() {
+    @Test
+    void reportTicketThreePlusHour() { //TEST 2 - Adding time afterwards
+        CarTicket testTicket = new CarTicket(3,LocalTime.of(17,59,59));
+        CheckoutStrategy checkoutTest = new NormalCheckout();
+
+        int reportedPrice = checkoutTest.reportTicket(testTicket,LocalTime.of(23,59,59));
+        int expectedPrice = checkoutTest.getBaseCost()+3; //The expected price should be the BASECOST (first three hours gets you to 20) + 3 dollars for another 3 hours
+        assertEquals(expectedPrice,reportedPrice,"Hours after first 3 did not calculate correctly, got "+reportedPrice+" from  method call instead of "+checkoutTest.getBaseCost());
     }
+
+    //Not adding a test for getters of finals
 }
